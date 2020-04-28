ee4c555e3599cd3c4909b171eecd70a740810b1b
ngecu <devngecu@gmail.com>
2020-04-28 14:10:41 +0300

    Added updated time

--- Projects/Selectable/chart.js
+++ Projects/Selectable/chart.js
@@ -29,25 +29,7 @@ var myChart = new Chart(ctx, {
                 'rgba(255, 206, 86, 1)',
             ],
             borderWidth: 1
-        },
-        {
-            label: 'Covid 19',
-            data: [data.confirmed.value, data.recovered.value, data.deaths.value],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
-                
-            ],
-            borderColor: [
-                '#ffd600',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-            ],
-            borderWidth: 1
-        },
-    
-    ]
+        }]
     },
     options: {
         scales: {
--- Projects/Selectable/leaflet.js
+++ Projects/Selectable/leaflet.js
@@ -4,7 +4,4 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mymap);
 
-L.geoJSON(abs, {
-    
-}).addTo(map);
 
