5ebbf44c85038e2a9e4a130b6b765bc07e870bc0
ngecu <devngecu@gmail.com>
2020-06-22 15:42:10 +0300

    trial

---
+++ Projects/pop/main/urls.py
@@ -0,0 +1,10 @@
+# todos/urls.py
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("",views.homepage,name="homepage"),
+    path("information/", views.information, name='information'),
+
+]
