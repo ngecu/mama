14c1f3cd0a5e0ca57acb54542d65e405cd14105e
ngecu <devngecu@gmail.com>
2020-06-22 18:26:49 +0300

    try

---
+++ Projects/pop/src/staticfiles/main/js/chartjs.js
@@ -0,0 +1,67 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+ 
+fetch('https://covid19.mathdro.id/api')
+// Replace ./data.json with your JSON feed
+.then(response => {
+return response.json()
+})
+.then(data => {
+// Work with JSON data here
+var myChart = new Chart(ctx, {
+    
+    type: 'bar',
+    data: {
+        labels: ['Confrimed', 'Recovered', 'Deaths'],
+        datasets: [{
+            label: 'World State',
+            data: [data.confirmed.value, data.recovered.value, data.deaths.value],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+                
+            ],
+            borderColor: [
+                '#ffd600',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        legend: {
+            labels: {
+                fontColor: "white",
+                fontSize: 18
+            }
+        },
+
+        scales: {
+            yAxes: [{
+                ticks: {
+                    fontColor: "white",
+                    beginAtZero: true
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    fontColor: "white",
+                    fontSize: 18,
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var confirmed = new CountUp("confirmedValue", 0,data.confirmed.value, 0, 2.5);
+confirmed.start();
+
+var recovered = new CountUp("recoveredValue", 0,data.recovered.value, 0, 2.5);
+recovered.start();
+
+var deaths = new CountUp("deathsValue", 0,data.deaths.value, 0, 2.5);
+deaths.start();
+
+})
